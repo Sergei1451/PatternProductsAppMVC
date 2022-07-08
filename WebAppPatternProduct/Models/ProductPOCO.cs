6be8f66cf71cc7d997d7f59c588993e35b29aa75
Сергей <@ADA>
2022-07-08 20:46:24 +0300

    Создал Модель ProductPOCO, Установил пакеты EntityFrameworkCore.Design и SQLServer

---
+++ WebAppPatternProduct/Models/ProductPOCO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppPatternProduct.Models
+{
+    public class ProductPOCO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? CategoryProduct { get; set; }
+        public decimal Price { get; set; }
+    }
+}
