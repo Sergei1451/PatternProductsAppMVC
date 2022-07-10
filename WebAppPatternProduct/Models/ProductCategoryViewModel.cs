48f80819ff8c1a646b851484b917b1f69a278600
Сергей <@ADA>
2022-07-10 23:54:27 +0300

    Заполнил созданный для поиска по категориям класс

--- WebAppPatternProduct/Models/ProductCategoryViewModel.cs
+++ WebAppPatternProduct/Models/ProductCategoryViewModel.cs
@@ -1,7 +1,12 @@
-﻿namespace WebAppPatternProduct.Models
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebAppPatternProduct.Models
 {
     public class ProductCategoryViewModel
     {
-
+        public List<ProductPOCO>? Products { get; set; }
+        public SelectList? Category { get; set; }
+        public string? ProductCategory { get; set; }
+        public string SearchString { get; set; }
     }
 }
