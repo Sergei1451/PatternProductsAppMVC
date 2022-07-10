b523011b8b8caaf7a653673e5149a3cfe6e5b48d
Сергей <@ADA>
2022-07-11 01:55:39 +0300

    Изменил модель так, что бы свойства получили защиту от ввода некорректных данных

--- WebAppPatternProduct/Models/ProductPOCO.cs
+++ WebAppPatternProduct/Models/ProductPOCO.cs
@@ -6,13 +6,23 @@ namespace WebAppPatternProduct.Models
     public class ProductPOCO
     {
         public int Id { get; set; }
+        [StringLength(60, MinimumLength = 6)]
+        [Required]
         public string Title { get; set; }
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string? CategoryProduct { get; set; }
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [StringLength(10)]
+        [Required]
         public string? Rating { get; set; }
     }
 }
